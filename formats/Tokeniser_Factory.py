9da8d6d5e5f455eaaf37a1598462674c1ef3bd66
Julian Brown <julian.brown@the-lair.info>
2021-01-13 11:37:05 +0000

    feat(trs80): add trs80 option to cli
    Signed-off-by: Julian Brown <julian.brown@the-lair.info>

--- formats/Tokeniser_Factory.py
+++ formats/Tokeniser_Factory.py
@@ -1,7 +1,8 @@
-from formats.Coco_Tokens import CoCoToken
 from formats.Coco_Rsdos_Tokens import RsDosToken
-from formats.Dragon_Tokens import DragonToken
+from formats.Coco_Tokens import CoCoToken
 from formats.Dragon_Dos_Tokens import DragonDosToken
+from formats.Dragon_Tokens import DragonToken
+from formats.Trs80_Tokens import Trs80Token
 
 
 def find_tokeniser(options):
@@ -13,4 +14,6 @@ def find_tokeniser(options):
             result = CoCoToken()
         elif any([op in ["-rd", "--rsdos"] for op in options]):
             result = RsDosToken()
+        elif any([op in ["-b2", "--basic2"] for op in options]):
+            result = Trs80Token()
     return result
